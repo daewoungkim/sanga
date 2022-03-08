ac6bb3a7665338efacfb82baa34f1815bcdd770b
dwkim <dwkim@enliple.com>
2022-03-08 15:02:35 +0900

    test222

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/about/{code}', [GoodsController::class, 'detail']);
 
 Route::prefix('zay')->group(function () {
     Route::get('/test', function () {
-        dd('testt');
+        dd('testtt');
     });
 
     Route::get('/', function () {
