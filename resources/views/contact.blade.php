8e446189d653c92a2f67f98e2540938b1fda47f5
dwkim <sosaeodn@gmail.com>
2022-08-28 17:58:48 +0900

    upload 구현중

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,30 +11,6 @@
     </div>
 </div>
 
-<!-- Start Map -->
-<div id="mapid" style="width: 100%; height: 300px;"></div>
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
-<script>
-    var mymap = L.map('mapid').setView([-23.013104, -43.394365, 13], 13);
-
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 18,
-        attribution: 'Zay Telmplte | Template Design by <a href="https://templatemo.com/">Templatemo</a> | Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1
-    }).addTo(mymap);
-
-    L.marker([-23.013104, -43.394365, 13]).addTo(mymap)
-        .bindPopup("<b>Zay</b> eCommerce Template<br />Location.").openPopup();
-
-    mymap.scrollWheelZoom.disable();
-    mymap.touchZoom.disable();
-</script>
-<!-- Ena Map -->
-
 <!-- Start Contact -->
 <div class="container py-5">
     <div class="row py-5">
