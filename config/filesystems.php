53f1ce21e2fe1f8c1284b68892e3808528287d01
dwkim <dwkim@enliple.com>
2022-09-08 11:46:42 +0900

    contact 개발중223222222

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,6 +40,16 @@ return [
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
+            'permissions' => [
+                'file' => [
+                    'public' => 0775,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0775,
+                    'private' => 0700,
+                ],
+            ],
         ],
 
         's3' => [
