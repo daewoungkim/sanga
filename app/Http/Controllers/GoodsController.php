be2b021fe9ed26a6f1203454d2d853dd769caeab
dwkim <dwkim@enliple.com>
2022-09-08 11:16:57 +0900

    contact 개발중22

--- app/Http/Controllers/GoodsController.php
+++ app/Http/Controllers/GoodsController.php
@@ -54,7 +54,7 @@ class GoodsController extends Controller
         $path = $file->storeAs(
             'contact', request()->name."_".date('ymdhis').".".$ext,['disk'=>'public']
         );
-        $url = asset("storage".$path);
+        $url = asset("storage/".$path);
 
         $data = [
             'name' => request()->name,
