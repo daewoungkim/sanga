bfb044561d23af220ecc56cdb2ecb296cf691bd6
dwkim <dwkim@enliple.com>
2022-11-02 10:01:06 +0900

    유튜브 임베딩4

--- resources/views/feane/about.blade.php
+++ resources/views/feane/about.blade.php
@@ -37,7 +37,7 @@
                     .embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; width:100%; }
                     .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
                 </style>
-                <div class='embed-container'><iframe src='https://www.youtube.com/embed/tvduY2aBnck' frameborder='0' allowfullscreen></iframe></div>
+                <div class='embed-container'><iframe src='https://www.youtube.com/embed/{{ $item->link }}' frameborder='0' allowfullscreen></iframe></div>
             </div>
         @endif
 
